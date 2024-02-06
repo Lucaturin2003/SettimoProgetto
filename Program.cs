d67a40008aaa943e1e6c3dc071c3f3658db909a9
Lucaturin2003 <159005233+Lucaturin2003@users.noreply.github.com>
2024-02-06 15:00:05 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Inerface
+{
+
+    public class ConsoleLogger : ILogger
+    {
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+    }
+    public interface ILogger
+    {
+        void LogError(string message);
+        void LogInfo(string message);
+    }
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.LogInfo("Migration started at {0}" + DateTime.Now);
+            _logger.LogInfo("Migration finished at {0}" + DateTime.Now);
+        }
+    }
+
+    public class FileLogger : ILogger
+    {
+        private readonly string _path;
+        public FileLogger(string path)
+        {
+            _path = path;
+        }
+        public void LogError(string message)
+        {
+            Log(message, "Error");
+        }
+
+        public void LogInfo(string message)
+        {
+            Log(message, "Info");
+        }
+
+        private void Log(string message, string messageType) 
+        {
+            using (var streamWriter = new StreamWriter(_path, true))
+            {
+                streamWriter.WriteLine(messageType + ": " + message);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new FileLogger("C\\log.txt"));
+            dbMigrator.Migrate();
+        }
+    }
+}
